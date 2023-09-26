4d1190f0af55ab99fb2e1a816a6d6725a2f0ac68
saumya9889 <msaumya114@gmail.com>
2023-09-26 21:38:56 +0530

    worked on header feature

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./assets/style/global.scss"; 
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,51 @@
+import React from "react";
+import flipkartlogo from "../assets/images/flipkartlogo.png";
+
+const Navbar = () => {
+  return (
+    <header>
+      <div className="container">
+        <nav className="bar">
+          <div className="left-side">
+          <div className="logo">
+            <img src={flipkartlogo} alt="logo" />
+            </div>
+            {/* <div className="explore-pluss">
+              <span> Explore</span>
+              <span>Plus</span>
+            </div> */}
+            <span className="search">
+              <b>
+                <i class="fas fa-search"></i>
+              </b>
+              <input
+                type="text"
+                placeholder="Search for Products, Brands and More"
+              />
+            </span>
+          </div>
+          <div className="right-side">
+           <div className="shop">
+           <i class="fa-solid fa-store"></i>
+           <button className="seller"><a href="/becomeaseller">Become a seller</a></button>
+           </div>
+           <div className="signup">
+           <i class="fa-regular fa-user"></i>
+           <button className="signin"><a href="/signin">Sign in</a></button>
+           </div>
+           <div className="carts">
+           <i class="fa-solid fa-cart-shopping"></i>
+           <button className="cart"><a href="/cart">cart</a></button>
+           </div>
+           <div className="dropdowns">
+           <i class="fa-solid fa-ellipsis-vertical"></i>
+           <button className="dropdown"><a href="/dropdown"> </a></button>
+           </div>
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
