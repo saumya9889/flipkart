5fc83bfb176499027ddb75e474cf3c1978a81b57
saumya9889 <msaumya114@gmail.com>
2024-04-29 22:12:52 +0530

    updated code

--- src/components/MegaMenuCard.js
+++ src/components/MegaMenuCard.js
@@ -1,3 +1,86 @@
+// import React from "react";
+// import MenuCard from "../components/MenuCard";
+// import Grocery from "../assets/images/grocery.png";
+// import Mobile from "../assets/images/mobile.png";
+// import Toys from "../assets/images/toys.png";
+// import Homes from "../assets/images/homes.png";
+// import Wheelers from "../assets/images/twowheelers.png";
+// import Fashion from "../assets/images/fashion.png";
+// import Electronics from "../assets/images/electronics.png";
+// import Travel from "../assets/images/travel.png";
+// import Appliances from "../assets/images/appliances.png";
+
+// const menuData = [
+//   {
+//     image: Grocery,
+//     title: "Grocery",
+//     icon: <i className="fa-solid fa-chevron-down"></i>,
+//   },
+//   { image: Mobile, title: "Mobiles" },
+//   { image: Fashion, title: "Fashion" },
+//   {
+//     image: Electronics,
+//     title: "Electronics",
+//     icon: <i className="fa-solid fa-chevron-down"></i>,
+//   },
+//   { image: Homes, title: "Home & Furniture" },
+//   { image: Appliances, title: "Appliances" },
+//   {
+//     image: Travel,
+//     title: "Travel",
+//   },
+//   { image: Toys, title: "beauty Toys & More" },
+//   { image: Wheelers, title: "Two Wheelers" },
+// ];
+
+// const megaMenuData = [
+//   { title: "Soups" , icon: <i className="fa-solid fa-chevron-right"></i>},
+//   { title: "Fuits" },
+//   { title: "Kitechen Things" },
+//   { title: "Household Things" },
+//   { title: "Washroom Things" },
+// ];
+// const megaMenuElectronicData = [
+// { title: "Phones" },
+// { title: "Head Phone" },
+// { title: "Tv's" },
+// { title: "Washing machines" },
+// { title: "Refrigerator" },
+// ];
+// const subGroceryData = [
+// { title: "Tide" },
+// { title: "Surf Excel" },
+// { title: "Dettol" },
+// { title: "Pears" },
+// { title: "LifeBuoy" },
+// ];
+
+
+
+// const MegaMenuCard = () => {
+//   return (
+//     <>
+//       <div className="menu-list bg-[#ffffff] flex gap-8 justify-center items-center mx-auto my-[0.6rem] w-w98 p-4 min-h-[126px]">
+//         {menuData.map((menu, index) => (
+//           <MenuCard
+//             key={index}
+//             image={menu.image}
+//             title={menu.title}
+//             icon={menu.icon}
+//             megaMenuData={megaMenuData}
+//             ElectronicData={megaMenuElectronicData}
+//             subGroceryData={subGroceryData}
+//           />
+//         ))}
+//       </div>
+//     </>
+//   );
+// };
+
+// export default MegaMenuCard;
+
+
+
 import React from "react";
 import MenuCard from "../components/MenuCard";
 import Grocery from "../assets/images/grocery.png";
@@ -23,14 +106,20 @@ const menuData = [
     title: "Electronics",
     icon: <i className="fa-solid fa-chevron-down"></i>,
   },
-  { image: Homes, title: "Home & Furniture" },
+  { image: Homes,
+    title: "Home & Furniture",
+    icon: <i className="fa-solid fa-chevron-down"></i>,
+     },
+
   { image: Appliances, title: "Appliances" },
   {
     image: Travel,
     title: "Travel",
+    icon: <i className="fa-solid fa-chevron-down"></i>,
   },
   { image: Toys, title: "beauty Toys & More" },
   { image: Wheelers, title: "Two Wheelers" },
+  
 ];
 
 const megaMenuData = [
@@ -55,7 +144,57 @@ const subGroceryData = [
 { title: "LifeBuoy" },
 ];
 
+const TravelData= [
+  { title: "Train" },
+  { title: "Flights" },
+  { title: "Bus" }, 
+  ];
 
+  const HomeData=[
+    {title:"Home Furnishing"},
+    {title:"Furniture Studio"},
+    { title: "Furniture" },
+    { title: "Home Decor" },
+    { title: "Home Appliances" },
+    { title: "Garden Tools" },
+    { title: "Kitchen Tools" },
+    { title: "Tools & Home Improvement" },
+    { title: " Kids Furniture" },
+  ]
+
+   const subHomeData=	[
+    {title:"Home Furnishing", icon: <i className="fa-solid fa-chevron-right"></i>},
+    {title:"Furniture Studio"},
+    { title: "Furniture" ,icon: <i className="fa-solid fa-chevron-right"></i>},
+    { title: "Home Decor" },
+    { title: "Home Appliances" },
+    { title: "Garden Tools" },
+    { title: "Kitchen Tools" },
+    { title: "Tools & Home Improvement" },
+    { title: " Kids Furniture" },
+    { title: "Bedding & Furnishing" },
+    { title: "Lighting" },
+    { title: "Decorative Access ories" },
+   ]
+
+   const homeFurnishingData=	[
+    {title:"Bedding & Furnishing"},
+    {title:"Lighting"},
+    { title: "Decorative Access ories" },
+   ]
+   const FurnitureData=	[
+    {title:"Furniture Studio"},
+    { title: "Furniture" },
+    { title: "Home Decor" },
+    { title: "Home Appliances" },
+    { title: "Garden Tools" },
+    { title: "Kitchen Tools" },
+    { title: "Tools & Home Improvement" },
+    { title: " Kids Furniture" },
+    { title: "Bedding & Furnishing" },
+    { title: "Lighting" },
+    { title: "Decorative Access ories" },
+   ]
 
 const MegaMenuCard = () => {
   return (
@@ -70,6 +209,11 @@ const MegaMenuCard = () => {
             megaMenuData={megaMenuData}
             ElectronicData={megaMenuElectronicData}
             subGroceryData={subGroceryData}
+            TravelData={TravelData}
+            HomeData={HomeData}
+            subHomeData={subHomeData}
+            homeFurnishingData={homeFurnishingData}
+            FurnitureData={FurnitureData}
           />
         ))}
       </div>
